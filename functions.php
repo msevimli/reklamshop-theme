be6a953b83511e74541d18cdfe0a31882b60cc0d
msevimli <muhammetsevimli06@gmail.com>
2020-08-24 22:21:58 +0200

    theme-development

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+// register front style
+add_action('wp_enqueue_scripts', 'front_require_styles');
+
+function front_require_styles()
+{
+    wp_enqueue_style('front-style', get_stylesheet_directory_uri().'/assets/css/front-style.css', true, 1.0);
+}
